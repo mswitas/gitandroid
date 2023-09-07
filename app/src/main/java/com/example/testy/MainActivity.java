39b644c4f0d19d735a6217fc118d1648cfec788d
mswitas <mswitas@gmail.com>
2023-09-07 16:44:12 +0200

    button activity added

--- app/src/main/java/com/example/testy/MainActivity.java
+++ app/src/main/java/com/example/testy/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.testy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textview;
+    Button button;
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textview = findViewById(R.id.textview);
+        button = findViewById(R.id.button);
+        textview.setText("" + count);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count++;
+                textview.setText("" + count);
+            }
+        });
+
     }
 }
